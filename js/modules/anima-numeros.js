30bccfed597ce8077ce4f66aa25a53c33bf4b3d7
“Pedro <pedrohenriquesampaiodenovaes@gmail.com>
2022-04-29 19:23:45 -0300

    Alteração no código anima-numeros. Função estava fora da function default

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,35 +1,33 @@
-export default function initAnimaisNumeros() {
+export default function initAnimaNumeros() {
+  function animaNumeros() {
+    const numeros = document.querySelectorAll('[data-numero]');
   
-}
-
-function animaNumeros() {
-  const numeros = document.querySelectorAll('[data-numero]');
-
-  numeros.forEach(numero => {
-    const numTotal = +numero.innerText;
-    const incremento = Math.round(numTotal / 100);
-
-    let numAtual = 0;
-    const intervalo = setInterval(() => {
-      numAtual = numAtual + incremento;
-      numero.innerText = numAtual;
-
-      if(numAtual > numTotal) {
-        numero.innerText = numTotal;
-        clearInterval(intervalo);
-      }
-    }, 25 * Math.random());
-  })
-}
-
-
-function handleObserver() {
-  if(observeTarget.classList.contains('ativo')) {
-    animaNumeros();
-    observer.disconnect();
+    numeros.forEach(numero => {
+      const numTotal = +numero.innerText;
+      const incremento = Math.round(numTotal / 100);
+  
+      let numAtual = 0;
+      const intervalo = setInterval(() => {
+        numAtual += incremento;
+        numero.innerText = numAtual;
+  
+        if(numAtual > numTotal) {
+          numero.innerText = numTotal;
+          clearInterval(intervalo);
+        }
+      }, 25 * Math.random());
+    })
   }
+  
+  
+  function handleObserver() {
+    if(observeTarget.classList.contains('ativo')) {
+      animaNumeros();
+      observer.disconnect();
+    }
+  }
+  
+  const observeTarget = document.querySelector('.numeros');
+  const observer = new MutationObserver(handleObserver);
+  observer.observe(observeTarget, {attributes: true});
 }
-
-const observeTarget = document.querySelector('.numeros');
-const observer = new MutationObserver(handleObserver);
-observer.observe(observeTarget, {attributes: true});
