1aaffdf60f8ad0520180379b746b420c1283bbd5
“Pedro <pedrohenriquesampaiodenovaes@gmail.com>
2022-04-30 16:57:10 -0300

    Alteração no script horario-funcionamento para retirar a conversão desnecessária de hora para minutos

--- js/modules/horario-funcionamento.js
+++ js/modules/horario-funcionamento.js
@@ -1,11 +1,11 @@
 export default function initHorarioFuncionamento() {
   const data = new Date();
   const diaAtual = data.getDay();
-  const horarioAtual = data.getHours() * 60;
+  const horarioAtual = data.getHours();
 
   const funcionamento = document.querySelector('[data-semana]');
   const diasSemana = funcionamento.dataset.semana.split(', ').map(Number);
-  const horarioSemana = funcionamento.dataset.horario.split(', ').map(hora => +hora * 60);
+  const horarioSemana = funcionamento.dataset.horario.split(', ').map(Number);
 
   const diaAberto = diasSemana.includes(diaAtual);
   const horarioAberto = (horarioSemana[0] <= horarioAtual && horarioAtual <= horarioSemana[1]);
